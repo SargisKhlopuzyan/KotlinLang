b94204a4c383077829036b92b64db567c7eeb71c
Sargis Khlopuzyan <sargis.khlopuzyan@org.fcc.am>
2020-02-14 10:07:55 +0400

    Enum Classes
    TODO - Working with Enum Constants

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/classesAndObjects/enumClasses/ImplementingInterfacesInEnumClasses.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/classesAndObjects/enumClasses/ImplementingInterfacesInEnumClasses.kt
@@ -9,6 +9,12 @@ package app.sargis.khlopuzyan.kotlinlang.classesAndObjects.enumClasses
 fun main() {
     val plus: IntArithmetic = IntArithmetic.valueOf("PLUS")
     val enumsArray: Array<IntArithmetic> = IntArithmetic.values()
+
+    println("plus: $plus")
+    println("enumsArray: ${enumsArray.toList()}")
+
+    // TODO
+    printAllValues<RGB>() // prints RED, GREEN, BLUE
 }
 
 
@@ -50,4 +56,15 @@ interface MyInterface2<T> {
 
 interface MyInterface3<T> {
     fun myInterfaceFunction3(t1: T, t2: T, t3: T)
+}
+
+
+// TODO - https://kotlinlang.org/docs/reference/enum-classes.html
+// TODO - Working with Enum Constants
+enum class RGB { RED, GREEN, BLUE }
+
+inline fun <reified T : Enum<T>> printAllValues() {
+
+    print(enumValues<T>().joinToString { it.name })
+
 }
