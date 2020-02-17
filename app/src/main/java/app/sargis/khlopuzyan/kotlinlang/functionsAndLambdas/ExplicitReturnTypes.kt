77fa30f1c349ced67c118f48d8560a07c95478b3
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-02-17 17:41:14 +0400

    Functions and Lambdas: Explicit return types : TODO

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/functionsAndLambdas/ExplicitReturnTypes.kt
@@ -0,0 +1,18 @@
+package app.sargis.khlopuzyan.kotlinlang.functionsAndLambdas
+
+/**
+ * Created by Sargis Khlopuzyan, on 2/17/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+/**
+ * Functions with block body must always specify return types explicitly, unless it's intended for them to return Unit,
+ * in which case it is optional. Kotlin does not infer return types for functions with block bodies because such
+ * functions may have complex control flow in the body,  and the return type will be non-obvious to the reader (and sometimes even for the compiler).
+ * */
+
+fun myFunction(x: Int, str: String, lambda: (Int, Int) -> Boolean) = lambda(x, x * x)
+
+fun myFunction2(x: Int, str: String, lambda: (Int, Int) -> Boolean) =
+    if (x > 2 * x) "positive" else "negative"
