3cd3b41b8edfb86acad31fac6a695fed4fc017d8
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-02-21 11:38:18 +0400

    Lambda expression syntax

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/functionsAndLambdas/lambdas/LambdaExpressionsAndAnonymousFunctions.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/functionsAndLambdas/lambdas/LambdaExpressionsAndAnonymousFunctions.kt
@@ -27,6 +27,10 @@ fun main() {
     })
     //Or
     val result3 = max("ARG_1", ::compare)
+
+
+    val result4 = sum2(5, 6)
+    println("result4: $result4")
 }
 
 fun max(arg1: String, lambda: (String, String) -> Boolean) {
@@ -36,3 +40,18 @@ fun max(arg1: String, lambda: (String, String) -> Boolean) {
 }
 
 fun compare(a: String, b: String): Boolean = a.equals(b, true)
+
+/**
+ * INFO-TODO
+ * Lambda expression syntax
+ *
+ * A lambda expression is always surrounded by curly braces, parameter declarations in the full syntactic
+ * form go inside curly braces and have optional type annotations, the body goes after an -> sign.
+ * If the inferred return type of the lambda is not Unit, the last (or possibly single) expression
+ * inside the lambda body is treated as the return value.
+ */
+//The full syntactic form of lambda expressions is as follows:
+val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+
+//If we leave all the optional annotations out, what's left looks like this:
+val sum2 = { x: Int, y: Int -> x + y }
