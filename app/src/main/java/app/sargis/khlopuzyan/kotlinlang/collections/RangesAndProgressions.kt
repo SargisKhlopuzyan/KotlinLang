e4298daa8043ea9ab028a667c541e3d2641fc393
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-02-24 15:12:26 +0400

    Progression

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/collections/RangesAndProgressions.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/collections/RangesAndProgressions.kt
@@ -9,6 +9,7 @@ package app.sargis.khlopuzyan.kotlinlang.collections
 fun main() {
     rangesAndProgressionsMain()
     rangeMain()
+    progressionMain()
 }
 
 fun rangesAndProgressionsMain() {
@@ -44,6 +45,7 @@ fun rangesAndProgressionsMain() {
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 /**
+ * Range
  * INFO-TODO
  *
  * To create a range for your class, call the rangeTo() function on the range start value and
@@ -65,3 +67,40 @@ class Version(var arg1: Int, var arg2: Int) : Comparable<Version> {
 
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
+/**
+ * Progression
+ * INFO-TODO
+ *
+ * As shown in the examples above, the ranges of integral types, such as Int, Long, and Char, can be treated as arithmetic progressions of them.
+ * In Kotlin, these progressions are defined by special types: IntProgression, LongProgression, and CharProgression.
+ *
+ * Progressions have three essential properties: the first element, the last element, and a non-zero step.
+ * The first element is first, subsequent elements are the previous element plus a step.
+ * Iteration over a progression with a positive step is equivalent to an indexed for loop in Java/JavaScript.
+ * */
+fun progressionMain() {
+
+    for (i in 1..10)
+        print(i)
+    println()
+
+    for (i in 1..8 step 2)
+        print(i)
+    println()
+
+    /**
+     * INFO-TODO
+     *
+     * The last element of the progression is calculated this way:
+     * For a positive step: the maximum value not greater than the end value such that (last - first) % step == 0.
+     * For a negative step: the minimum value not less than the end value such that (last - first) % step == 0.
+     * */
+
+    /**
+     * INFO-TODO
+     *
+     * Progressions implement Iterable<N>, where N is Int, Long, or Char respectively,
+     * so you can use them in various collection functions like map, filter, and other.
+     * */
+    println((1..10).filter { it % 2 == 0 })
+}
