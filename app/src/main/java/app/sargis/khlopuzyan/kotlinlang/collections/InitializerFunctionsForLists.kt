727392999368a683ffcfb938847b0b3d1ec5991e
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-02-24 12:32:07 +0400

    Initializer functions for lists

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/collections/InitializerFunctionsForLists.kt
@@ -0,0 +1,14 @@
+package app.sargis.khlopuzyan.kotlinlang.collections
+
+/**
+ * Created by Sargis Khlopuzyan, on 2/24/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+fun main() {
+    /**
+     * For lists, there is a constructor that takes the list size and the initializer function that defines the element value based on its index.
+     * */
+    val doubled = List(3, { it * 2 })  // or MutableList if you want to change its content later
+    println(doubled)
+}
