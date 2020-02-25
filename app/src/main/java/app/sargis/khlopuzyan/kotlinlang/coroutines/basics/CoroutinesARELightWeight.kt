c2e24d452a5e888d2407dfde3515c6b8f33a9d78
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-02-25 16:13:55 +0400

    Coroutine Basics: Global coroutines are like daemon threads

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/coroutines/basics/CoroutinesARELightWeight.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/coroutines/basics/CoroutinesARELightWeight.kt
@@ -1,5 +1,6 @@
 package app.sargis.khlopuzyan.kotlinlang.coroutines.basics
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -22,11 +23,33 @@ fun main2() = runBlocking {
 }
 
 
-fun main() = runBlocking {
+fun main3() = runBlocking {
     repeat(100_000) {
         thread {
             Thread.sleep(2000L)
             print(".")
         }
     }
+}
+
+// Global coroutines are like daemon threads
+/**
+ * The following code launches a long-running coroutine in GlobalScope that prints
+ * "I'm sleeping" twice a second and then returns from the main function after some delay:
+ * */
+fun main() = runBlocking {
+    GlobalScope.launch {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // just quit after delay
+    /**
+     * I'm sleeping 0 ...
+     * I'm sleeping 1 ...
+     * I'm sleeping 2 ...
+     *
+     * Active coroutines that were launched in GlobalScope do not keep the process alive. They are like daemon threads.
+     * */
 }
