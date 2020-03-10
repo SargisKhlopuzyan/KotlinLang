06b9a1acb0464f53e70146981b087252c6aefc22
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2020-03-10 10:33:07 +0400

    Retrieving Collection Parts : Slice

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/collections/retrievingCollectionParts/Slice.kt
@@ -0,0 +1,20 @@
+package app.sargis.khlopuzyan.kotlinlang.collections.retrievingCollectionParts
+
+/**
+ * Created by Sargis Khlopuzyan, on 3/10/2020.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fcc.am)
+ */
+
+/**
+ * Slice
+ *
+ * slice() returns a list of the collection elements with given indices.
+ * The indices may be passed either as a range or as a collection of integer values
+ * */
+fun main() {
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    println(numbers.slice(1..3))
+    println(numbers.slice(0..4 step 2))
+    println(numbers.slice(setOf(3, 5, 0)))
+}
