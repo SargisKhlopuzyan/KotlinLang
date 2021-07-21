341dc472b2d32a1d28e8ee716aa70ab0ec1c35e0
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-21 17:21:38 +0400

    Timeout

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/Timeout.kt
@@ -0,0 +1,47 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/21/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+
+fun main() {
+
+//    runBlocking {
+//        withTimeout(1300) {
+//            repeat(1000) { i ->
+//                println("I'm sleeping $i ...")
+//                delay(500)
+//            }
+//        }
+//    }
+
+    runBlocking {
+        val result = withTimeoutOrNull(1300) {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500)
+            }
+            "Done"
+        }
+        println("Result is $result")
+    }
+
+    runBlocking {
+        val result = withTimeoutOrNull(1300) {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+//                delay(500)
+            }
+            "Done"
+        }
+        println("Result is $result")
+    }
+
+
+}
