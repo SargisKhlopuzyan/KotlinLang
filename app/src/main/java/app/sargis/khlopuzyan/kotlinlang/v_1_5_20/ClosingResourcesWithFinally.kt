8f5e62ede2822e7ed1da0fd0e47ec5ba7927bac0
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-21 12:37:05 +0400

    Closing Resources With Finally

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/ClosingResourcesWithFinally.kt
@@ -0,0 +1,31 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/21/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("job: I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                println("job: I'm running finally")
+            }
+        }
+
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin()
+        println("main: Now I can quit.")
+    }
+}
