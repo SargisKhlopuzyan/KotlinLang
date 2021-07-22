50a68cc394175d7c20e0a208ab2bce29e3f82a94
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-22 16:59:10 +0400

    Async-style functions

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/composingSuspendingFunctions/AsyncStyleFunctions.kt
@@ -0,0 +1,47 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.composingSuspendingFunctions
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/22/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+
+    val time = measureTimeMillis {
+        // we can initiate async actions outside of a coroutine
+        val one = somethingUsefulOneAsync()
+        val two = somethingUsefulTwoAsync()
+        // but waiting for a result must involve either suspending or blocking.
+        // here we use `runBlocking { ... }` to block the main thread while waiting for the result
+        runBlocking {
+            println("The answer is ${one.await() + two.await()}")
+        }
+    }
+
+    println("Completed in $time ms")
+}
+
+// The result type of somethingUsefulOneAsync is Deferred<Int>
+@OptIn(DelicateCoroutinesApi::class)
+fun somethingUsefulOneAsync() = GlobalScope.async {
+    doSomethingUsefulOne4()
+}
+
+// The result type of somethingUsefulTwoAsync is Deferred<Int>
+@OptIn(DelicateCoroutinesApi::class)
+fun somethingUsefulTwoAsync() = GlobalScope.async {
+    doSomethingUsefulTwo()
+}
+
+suspend fun doSomethingUsefulOne4(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo4(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
+}
