8e23cc39d67716f906b812f8537f61dbb33ec4e5
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-22 14:59:19 +0400

    Asynchronous Timeout And Resources, Run Non-Cancellable Block

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/AsynchronousTimeoutAndResources.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/AsynchronousTimeoutAndResources.kt
@@ -25,38 +25,44 @@ class Resource {
 
 fun main() {
     runBlocking {
-        repeat(100_000) { // Launch 100K coroutines
+        repeat(1_000_000) { // Launch 100K coroutines
             launch {
+//                try{
                 val resource = withTimeout(60) { // Timeout of 60 ms
                     delay(50) // Delay for 50 ms
                     Resource() // Acquire a resource and return it from withTimeout block
                 }
                 resource.close() // Release the resource
+//                } catch (e: Exception) {
+//                    println("Exception : $e")
+//                }
             }
         }
     }
+
     // Outside of runBlocking all coroutines have completed
     println(acquired) // Print the number of resources still acquired
 
-    println("** ** ** ** ** ** ** ** ** ** **")
-
-    runBlocking {
-        repeat(100_000) { // Launch 100K coroutines
-            launch {
-                var resource: Resource? = null // Not acquired yet
-                try {
-                    withTimeout(60) { // Timeout of 60 ms
-                        delay(50) // Delay for 50 ms
-                        resource = Resource() // Acquire a resource and return it from withTimeout block
-                    }
-                    // We can do something else with the resource here
-                } finally {
-                    resource?.close() // Release the resource if it was acquired
-                }
-            }
-        }
-    }
-    // Outside of runBlocking all coroutines have completed
-    println(acquired) // Print the number of resources still acquired
+//    println("** ** ** ** ** ** ** ** ** ** **")
+//
+//    runBlocking {
+//        repeat(1_000_000) { // Launch 100K coroutines
+//            launch {
+//                var resource: Resource? = null // Not acquired yet
+//                try {
+//                    withTimeout(60) { // Timeout of 60 ms
+//                        delay(50) // Delay for 50 ms
+//                        resource =
+//                            Resource() // Acquire a resource and return it from withTimeout block
+//                    }
+//                    // We can do something else with the resource here
+//                } finally {
+//                    resource?.close() // Release the resource if it was acquired
+//                }
+//            }
+//        }
+//    }
+//    // Outside of runBlocking all coroutines have completed
+//    println(acquired) // Print the number of resources still acquired
 
 }
--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/RunNonCancellableBlock.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/RunNonCancellableBlock.kt
@@ -8,6 +8,31 @@ import kotlinx.coroutines.*
  * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
  */
 fun main() {
+
+    runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("job: I'm in sleeping $i ...")
+                    delay(500)
+                }
+            } finally {
+                try {
+                    suspendFunctionInFinal()
+                } catch (e: CancellationException) {
+                    println("e: $e")
+                }
+            }
+        }
+
+        delay(1300) // delay a bit
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin()
+        println("main: Now I can quit.")
+    }
+
+    println("** ** ** ** ** ** ** ** ** ** ** **")
+
     runBlocking {
         val job = launch {
             try {
@@ -28,5 +53,16 @@ fun main() {
         println("main: I'm tired of waiting!")
         job.cancelAndJoin()
         println("main: Now I can quit.")
+    }
+}
+
+suspend fun suspendFunctionInFinal() {
+    coroutineScope {
+        launch {
+            repeat(100) {
+                delay(2000)
+                println("main: I am in suspendFunctionInFinal")
+            }
+        }
     }
 }
