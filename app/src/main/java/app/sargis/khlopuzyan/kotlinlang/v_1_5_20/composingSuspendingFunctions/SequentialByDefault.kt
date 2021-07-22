6ff59caf5609916dd9144d99bac96fc6eaebdae3
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-22 15:11:46 +0400

    Sequential By Default

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/composingSuspendingFunctions/SequentialByDefault.kt
@@ -0,0 +1,31 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.composingSuspendingFunctions
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/22/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking<Unit> {
+        val time = measureTimeMillis {
+            val one = doSomethingUsefulOne()
+            val two = doSomethingUsefulTwo()
+            println("The answer is ${one + two}")
+        }
+        println("Completed in $time ms")
+    }
+}
+
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
+}
