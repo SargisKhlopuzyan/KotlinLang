802afbb9d752ffaea756cfa2532b9eb5ae234d48
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-23 15:09:17 +0400

    Parental responsibilities

--- app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/debuggingCoroutinesAndThreads/ChildrenOfACoroutine.kt
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/debuggingCoroutinesAndThreads/ChildrenOfACoroutine.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.runBlocking
  */
 fun main() {
     runBlocking {
-
         // launch a coroutine to process some kind of incoming request
         val request = launch {
             // it spawns two other jobs
@@ -34,5 +33,35 @@ fun main() {
         request.cancel() // cancel processing of the request
         delay(1000) // delay a second to see what happens
         println("main: Who has survived request cancellation?")
+    }
+
+
+    // version 2
+    runBlocking {
+
+        var request2: Job? = null
+
+        // launch a coroutine to process some kind of incoming request
+        val request = launch {
+            // it spawns two other jobs
+            request2 = launch(Job()) {
+                println("job1: I run in my own Job and execute independently!")
+                delay(2000)
+                println("job1: I am not affected by cancellation of the request")
+            }
+            // and the other inherits the parent context
+            launch {
+                delay(100)
+                println("job2: I am a child of the request coroutine")
+                delay(1000)
+                println("job2: I will not execute this line if my parent request is cancelled")
+            }
+        }
+
+        delay(500)
+        request.cancel() // cancel processing of the request
+        delay(1000) // delay a second to see what happens
+        println("main: Who has survived request cancellation?")
+        request2?.join()
     }
 }
---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/debuggingCoroutinesAndThreads/ParentalResponsibilities.kt
@@ -0,0 +1,27 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.debuggingCoroutinesAndThreads
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/23/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        // launch a coroutine to process some kind of incoming request
+        val request = launch {
+            repeat(3) { i -> // launch a few children jobs
+                launch {
+                    delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+                    println("Coroutine $i is done")
+                }
+            }
+            println("request: I'm done and I don't explicitly join my children that are still active")
+        }
+        request.join() // wait for completion of the request, including all its children
+        println("Now processing of the request is complete")
+    }
+}
