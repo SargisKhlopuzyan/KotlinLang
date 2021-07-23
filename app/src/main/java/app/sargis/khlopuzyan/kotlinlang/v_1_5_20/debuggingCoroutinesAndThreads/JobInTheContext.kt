37851075c04b4e7f9d9c24f5422b8055590d82a7
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-23 14:26:16 +0400

    Job in the context

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/debuggingCoroutinesAndThreads/JobInTheContext.kt
@@ -0,0 +1,15 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.debuggingCoroutinesAndThreads
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/23/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        println("My job is ${coroutineContext[Job]}")
+    }
+}
