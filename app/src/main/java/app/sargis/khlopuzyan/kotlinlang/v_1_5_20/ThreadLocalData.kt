fc2b3d47024d39905710239dbd886863f14fd86d
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-24 11:27:19 +0400

    Thread-local data

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/ThreadLocalData.kt
@@ -0,0 +1,24 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20
+
+import kotlinx.coroutines.*
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/24/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        threadLocal.set("main")
+        println("Pre-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+        val job = launch(Dispatchers.Default + threadLocal.asContextElement(value = "launch")) {
+            println("Launch start, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+            yield()
+            println("After yield, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+        }
+        job.join()
+        println("Post-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+    }
+}
+
+val threadLocal = ThreadLocal<String?>() // declare thread-local variable
