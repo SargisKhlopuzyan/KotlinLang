5e19e2fcbb731f41d865ca6317e80e713d41f296
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-07-27 16:44:32 +0400

    flatMapLatest

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/asynchronousFlow/flatteningFlows/FlatMapLatest.kt
@@ -0,0 +1,27 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.asynchronousFlow.flatteningFlows
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Created by Sargis Khlopuzyan, on 7/27/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        val startTime = System.currentTimeMillis() // remember the start time
+        (1..3).asFlow().onEach { delay(100) } // a number every 100 ms
+            .flatMapLatest { requestFlow4(it) }
+            .collect { value -> // collect and print
+                println("$value at ${System.currentTimeMillis() - startTime} ms from start")
+            }
+    }
+}
+
+fun requestFlow4(i: Int): Flow<String> = flow {
+    emit("$i: First")
+    delay(500) // wait 500 ms
+    emit("$i: Second")
+}
