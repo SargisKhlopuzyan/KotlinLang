d41563db6c443625240fd9de994b8c8a1f02cf97
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-08-05 11:18:02 +0400

    Fan-in

---
+++ app/src/main/java/app/sargis/khlopuzyan/kotlinlang/v_1_5_20/channels/FanIn.kt
@@ -0,0 +1,32 @@
+package app.sargis.khlopuzyan.kotlinlang.v_1_5_20.channels
+
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Created by Sargis Khlopuzyan, on 8/5/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    runBlocking {
+        val channel = Channel<String>()
+        launch { sendString(channel, "foo", 200L) }
+        launch { sendString(channel, "BAR!", 500L) }
+        repeat(6) { // receive first six
+            println(channel.receive())
+        }
+        coroutineContext.cancelChildren() // cancel all children to let main finish
+    }
+}
+
+suspend fun sendString(channel: SendChannel<String>, s: String, time: Long) {
+    while (true) {
+        delay(time)
+        channel.send(s)
+    }
+}
